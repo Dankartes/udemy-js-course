a8696b8c84500a805e595893a69614a6d0fb24f0
Robert <c.robertstancu@yahoo.com>
2023-06-20 21:14:08 +0300

    section 16

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,46 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+//https://countries-api-836d.onrender.com/countries/
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+
+    console.log(data);
+
+    const flag = data.flags.png;
+    const name = data.name.common;
+    const region = data.region;
+    const population = (data.population / 1000000).toFixed(1);
+
+    const languages = Object.getOwnPropertyNames(data.languages);
+    const { [languages[0]]: language } = data.languages;
+
+    const { [Object.keys(data.currencies)[0]]: currency } = data.currencies;
+
+    const html = `<article class="country">
+            <img class="country__img" src="${flag}" />
+            <div class="country__data">
+              <h3 class="country__name">${name}</h3>
+              <h4 class="country__region">${region}</h4>
+              <p class="country__row"><span>👫</span>${population}M people</p>
+              <p class="country__row"><span>🗣️</span>${language}</p>
+              <p class="country__row"><span>💰</span>${currency.name}</p>
+            </div>
+          </article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+
+getCountryData(`china`);
+
